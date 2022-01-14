65fadacec4383eaf275c7cbd1dc92fc221f5d5dd
alaJream <jeremy.gonzales@students.dominican.edu>
2022-01-13 21:19:33 -0800

    init commit

---
+++ app/__init__.py
@@ -0,0 +1,60 @@
+# from whitenoise import WhiteNoise
+from flask import Flask, render_template
+
+
+# portfolio = ("portfolio", __name__, template_folder="templates")
+portfolio = Flask(__name__)
+
+@portfolio.route("/")
+def index():
+    title = "Portfolio"
+    description = "About Me"
+    return render_template(
+        "index.html",
+        title=title,
+        description=description,
+    )
+
+
+@portfolio.route("/projects/")
+def projects():
+    title = "Projects"
+    description = "Project"
+    return render_template(
+        "projects.html",
+        title=title,
+        description=description,
+    )
+
+
+@portfolio.route("/contact/")
+def contact():
+    title = "Contact"
+    description = "contact"
+    return render_template(
+        "contact.html",
+        title=title,
+        description=description,
+    )
+
+
+@portfolio.route("/resume/")
+def resume():
+    title = "Resume"
+    description = "Resume"
+    return render_template(
+        "resume.html",
+        title=title,
+        description=description,
+    )
+
+
+
+
+@portfolio.errorhandler(404)
+def not_found(error_message):
+    return render_template("404.html")
+
+
+if __name__ == "__main__":
+    portfolio.run()
